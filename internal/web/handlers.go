38680ae78b733ada0f4616af2f4eb56fba3c1844
igor <gsambir519@gmail.com>
2023-01-21 09:44:22 +0200

    auth login and register

--- internal/web/handlers.go
+++ internal/web/handlers.go
@@ -7,13 +7,10 @@ import (
 func HandleRequests(){
 	http.HandleFunc("/api/v1/elements",Elements)
 	http.HandleFunc("/api/v1/houses",Houses)
-	// http.HandleFunc("/sign",SignPage)
-	// http.HandleFunc("/login",LoginPage)
 
-	// http.HandleFunc("/ajax/posts",PostsAjax)
-	// http.HandleFunc("/ajax/chats",ChatsAjax)
-	// http.HandleFunc("/ajax/chatMessages",ChatMessagesAjax)
-	
+	http.HandleFunc("/api/v1/user/register",RegisterUser)
+	http.HandleFunc("/api/v1/user/login",LoginUser)
+
 	// http.HandleFunc("/ws/chat",ws_chat)
 
 	// http.Handle("/static/",http.StripPrefix("/static/", http.FileServer(http.Dir("../../ui_old_files/static"))))
