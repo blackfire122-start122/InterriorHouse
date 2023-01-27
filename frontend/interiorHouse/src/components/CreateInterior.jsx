d0e4b9ac217f10e7554a74265f543107a33435b6
igor <gsambir519@gmail.com>
2023-01-27 12:32:33 +0200

    CreateInteriorPage

--- frontend/interiorHouse/src/components/CreateInterior.jsx
+++ frontend/interiorHouse/src/components/CreateInterior.jsx
@@ -1,5 +1,10 @@
 import React, {useState, useEffect} from 'react';
 
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+
+import { DoubleSide, MathUtils } from "three";
+
 import ElementList from './createInteriorComponents/ElementList'
 
 import './../styles/createInterior/createInterior.css';
@@ -7,21 +12,36 @@ import './../styles/createInterior/createInterior.css';
 const CreateInterior = function ({client}) {
     const [elements,setElements] = useState([])
 
+    useEffect(() => {
+        fetchElements()
+        // eslint-disable-next-line
+    }, [])
+
     async function fetchElements() {
         const response = await client.get("/elements")
         setElements(response.data)
     }
 
-    useEffect(() => {
-        fetchElements()
-    }, [])
-
 	return(
 		<div className="createInterior">
-            <h1>create</h1>
+            <Canvas style={{height: `100%`, position: `absolute`, top:`0` }}>
+                <GreenSquare />
+                <ambientLight />
+                <PerspectiveCamera position={[2, 2, 2]} makeDefault />
+                <OrbitControls />
+            </ Canvas>
             <ElementList elements={elements} />
         </div>
 	)
+}
+
+function GreenSquare() {
+  return (
+    <mesh position={[0, 0, 0]} rotation={[Math.PI / 2, 0, MathUtils.degToRad(45)]} scale={[5, 5, 5]}>
+      <planeBufferGeometry />
+      <meshBasicMaterial color="green" side={DoubleSide} />
+    </mesh>
+  );
 }
 
 export default CreateInterior;
--- frontend/interiorHouse/src/components/Main.jsx
+++ frontend/interiorHouse/src/components/Main.jsx
@@ -4,15 +4,16 @@ import HouseList from './HouseList'
 
 const Main = function ({client}) {
 	const [houses,setHouses] = useState([])
+    
+    useEffect(() => {
+        fetchHouse()
+        // eslint-disable-next-line
+    }, [])
 
     async function fetchHouse() {
         const response = await client.get("/houses")
         setHouses(response.data)
     }
-
-    useEffect(() => {
-        fetchHouse()
-    }, [])
 
 	return(
 		<HouseList houses={houses} />
